449dddf61fd3971552bc37f64aa441f16e3cacbe
Nsharifi650 <91625493+Nsharifi650@users.noreply.github.com>
2025-03-22 17:29:41 +0000

    model training complete

--- src/train.py
+++ src/train.py
@@ -1,7 +1,16 @@
 import os
+
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn as nn
+
+from src.model import Transformer
+from src.dataset import load_dataset
+from utils.logging_config import get_logger
+
+logger = get_logger(__name__)
 
 def create_padding_mask(seq):
     seq_masked = torch.tensor(seq) == 0 # True if value is 0 otherwise false
@@ -12,70 +21,95 @@ def create_look_ahead_mask(size):
     mask = torch.triu(torch.ones((size, size)), diagonal=1) 
     return mask.unsqueeze(0).unsqueeze(1)
 
-
-import torch.optim as optim
-import torch.nn as nn
-
 def loss_function(real, pred):
     mask = real != 0
     loss = nn.CrossEntropyLoss(reduction='none')(pred.transpose(1, 2), real)
     mask = mask.float()
     loss *= mask
-
     return torch.mean(loss)
 
-def train_model(transformer, train_loader, num_epochs, learning_rate, model_name, pretrained):
+def train_model(
+    model: Transformer,
+    train_loader: DataLoader,
+    test_loader: DataLoader,
+    num_epochs: int,
+    learning_rate: float,
+    model_name: str,
+    pretrained: bool,
+    save_freq: int = 2,
+    ) -> None:
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     if pretrained:
-        transformer.load_state_dict(torch.load(model_name))
-    optimizer = optim.Adam(transformer.parameters(), lr=learning_rate)
+        model.load_state_dict(torch.load(model_name))
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    model = model.to(device)
     
     for epoch in range(num_epochs):
-        transformer.train()
+        model.train()
         total_loss = 0
         
-        for idx_num, (properties, smiles) in enumerate(train_loader):
-   
+        for properties, smiles in train_loader:
             properties = properties.to(device)
             smiles = smiles.to(device)
             look_ahead_mask = create_look_ahead_mask(smiles.size(1))
             dec_padding_mask = create_padding_mask(smiles)
             
             optimizer.zero_grad()
-            predictions = transformer(properties, smiles, look_ahead_mask, dec_padding_mask, training=True)
+            predictions = model(properties, smiles, look_ahead_mask, dec_padding_mask, training=True)
             loss = loss_function(smiles[:, 1:], predictions[:, :-1])
-            
             loss.backward()
             optimizer.step()
             
             total_loss += loss.item()
-            print("batch loss", loss.item())
-            # save model at the end of each epoch
-            torch.save(transformer.state_dict(), model_name)
-            
-        print(f'Epoch {epoch+1}, Loss: {total_loss / (idx_num + 1)}')
 
-# Initialize the model
-target_vocab_size = len(char_to_idx)
-print("target vocab size", target_vocab_size)
-num_layers = 8
-enc_d_model = 5 # number of properties
-dec_d_model = 128
-enc_num_heads = 1
-dec_num_heads = 8
-enc_dff = 128 # dimension of the feed forward layer
-dec_dff = enc_dff
-pe_target = 1000 # positional encoding
-model_name = "molecularTransformer2.pth"
-learning_rate = 1e-5
-num_epochs = 20
-pretrained = True
+        with torch.no_grad():
+            test_error = 0
+            model.eval()
+            for properties, smiles in test_loader:
+                properties = properties.to(device)
+                smiles = smiles.to(device)
+                look_ahead_mask = create_look_ahead_mask(smiles.size(1))
+                dec_padding_mask = create_padding_mask(smiles)
+                predictions = model(properties, smiles, look_ahead_mask, dec_padding_mask, training=True)
+                loss = loss_function(smiles[:, 1:], predictions[:, :-1])
+                test_error += loss.item()
 
-transformer = Transformer(num_layers, enc_d_model, dec_d_model,
-                          enc_num_heads, dec_num_heads, enc_dff, 
-                          dec_dff, target_vocab_size, pe_target)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-transformer = transformer.to(device)
+        print(f'Epoch {epoch+1}, Training Loss: {total_loss / len(train_loader)}, validation Loss: {test_error/ len(test_loader)}')
+        if (epoch+1) % save_freq == 0:
+            torch.save(model.state_dict(), os.path.join("saved_models", model_name))
 
-# Train the model
-train_model(transformer, train_loader, num_epochs, learning_rate, model_name, pretrained)
+def run_training_pipeline(
+        dataset_dir: str,
+        batch_size: int,
+        config
+        ) -> None:
+    train_loader, test_loader, char_to_idx, idx_to_char = load_dataset(
+        dataset_dir, batch_size)
+    
+    # instantiate the model
+    target_vocab_size = len(char_to_idx)
+    num_layers = 8
+    enc_d_model = 5 # number of properties
+    dec_d_model = 128
+    enc_num_heads = 1
+    dec_num_heads = 8
+    enc_dff = 128 # dimension of the feed forward layer
+    dec_dff = enc_dff
+    pe_target = 1000 # positional encoding
+    model_name = "molecularTransformer2.pth"
+    learning_rate = 1e-5
+    num_epochs = 20
+    pretrained = True
+    save_freq = 2
+    transformer = Transformer(num_layers, enc_d_model, dec_d_model,
+                            enc_num_heads, dec_num_heads, enc_dff, 
+                            dec_dff, target_vocab_size, pe_target)
+    
+    logger.info("Model training starting...")
+    train_model(transformer, train_loader, test_loader,
+                num_epochs, learning_rate, model_name, 
+                pretrained, save_freq
+            )
+    logger.info("Model training complete")
