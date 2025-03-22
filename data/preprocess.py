f40e5da889e14650382aec47276e9b094de27ee0
Nsharifi650 <91625493+Nsharifi650@users.noreply.github.com>
2025-03-22 15:09:39 +0000

    data preprocessing done

---
+++ data/preprocess.py
@@ -0,0 +1,67 @@
+import os
+
+import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from utils.logging_config import get_logger
+
+logger = get_logger(__name__)
+
+def save_dataset(
+        properties: list[float],
+        smiles_indices: list[int],
+        idx_to_char: dict[int, str],
+        test_size: float,
+        scaler,
+        output_dir: str
+    ) -> None:
+    os.makedirs(output_dir, exist_ok=True)
+    train_props, test_props, train_smiles, test_smiles = train_test_split(properties, smiles_indices, test_size=test_size)
+
+    with open(os.path.join(output_dir, "train_dateset.pkl"), 'wb') as f:
+        pickle.dump((train_props, train_smiles), f)
+    
+    with open(os.path.join(output_dir, "test_dataset.pkl"), 'wb') as f:
+        pickle.dump((test_props, test_smiles), f)
+    
+    with open(os.path.join(output_dir, "char_mappings.pkl"), "wb") as f:
+        pickle.dump((idx_to_char, idx_to_char), f)
+
+    with open(os.path.join(output_dir, "scaler.pkl"), "wb") as f:
+        pickle.dump(scaler, f)
+    
+    logger.info(f"Saved preprocessing datasets and mappings in the directory: {output_dir}")
+
+
+def preprocess_data(csv_file: str) -> tuple[list[float],list[int],dict[str,int], any]:
+    data = pd.read_csv(csv_file)
+    properties = data[['polararea', 'complexity', 'heavycnt', 'hbonddonor', 'hbondacc']].values
+    smiles = data['isosmiles'].values
+    logger.info("length of smiles: ", smiles.shape)
+    # print(f"smiles: {smiles}")
+
+    scaler = StandardScaler()
+    properties = scaler.fit_transform(properties)
+    
+    # Convert SMILES to a list of character indices
+    char_to_idx = {char: idx + 3 for idx, char in enumerate(sorted(set(''.join(smiles))))}
+    char_to_idx['<pad>'] = 0
+    char_to_idx['<start>'] = 1
+    char_to_idx['<end>'] = 2
+
+    idx_to_char = {idx: char for char, idx in char_to_idx.items()}
+    
+    max_smiles_len = max(len(s) for s in smiles)+2 # 2 for the start and end token
+    logger.info(f"max smiles length: {max_smiles_len}")
+    smiles_indices = [
+        [char_to_idx['<start>']] + [char_to_idx[char] for char in smi] + [char_to_idx['<end>']] + 
+        [char_to_idx['<pad>']] * (max_smiles_len - len(smi) - 2)
+        for smi in smiles
+    ]
+    logger.info("Data processing: conversion of SMILEs notation of molecules to sequence complete")
+
+    return properties, smiles_indices, idx_to_char, scaler
+
+
